4fa7139572b40b59c66b0f3d498a8fc00c2d4f45
Hikaru Yoshida <guangjitian755@gmail.com>
2025-11-30 20:14:18 +0900

    テスト内容修正

--- test.bash
+++ test.bash
@@ -13,12 +13,12 @@ res=0
 
 #NOMAL INPUT
 out=$(echo -e "2 1 1\n1 2 1\n0 1 2" | ./inverse)
-expected=$'0.600 -0.400 0.200\n-0.200 0.800 -0.400\n-0.200 -0.200 0.600'
+expected=$'0.600 -0.200 -0.200\n-0.400 0.800 -0.200\n0.200 -0.400 0.600'
 [ "${out}" = "${expected}" ] || ng "$LINENO"
 
 # STRANGE INPUT 
 out=$(echo -e "1 2 3\n4 5 6\n7 8 9" | ./inverse)
-[ "${out}" = "逆行列は存在しない" ] || ng "$LINENO"
+[ "${out}" = "ERROR" ] || ng "$LINENO"
 
 out=$(echo -e "1 2\n1 1" | ./inverse)
 [ "$?" = 1 ] || ng "$LINENO"
